0ffce49e53b6bdb4fc039be93b4c816ed657f2bd
Grant Hughes <granthughes636@gmail.com>
2025-10-09 23:09:00 -0500

    added more code and getting ready to change main

--- PROJECT_3/oss.cpp
+++ PROJECT_3/oss.cpp
@@ -7,6 +7,7 @@ File: This program manages child process with a message queue
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <unistd.h>
@@ -110,3 +111,93 @@ void log_output(const string& message)
     }
 }
 
+void incrementClock(SimulatedClock* clock, int activeChildren) 
+{
+    // 250ms/number of children
+    int increment = (activeChildren > 0) ? (250000000 / activeChildren) : 250000000;
+    clock->nanoSeconds += increment;
+    if (clock->nanoSeconds >= 1000000000) 
+    {
+        clock->seconds++;
+        clock->nanoSeconds -= 1000000000;
+    }
+}
+
+bool isTimeToLaunch(SimulatedClock* clock, int lastLaunchS, int lastLaunchN, double interval) 
+{
+    int intervalSeconds = (int)interval;
+    int intervalNano = (int)((interval - intervalSeconds) * 1000000000);
+    
+    long long currentTime = (long long)clock->seconds * 1000000000 + clock->nanoSeconds;
+    long long lastLaunchTime = (long long)lastLaunchS * 1000000000 + lastLaunchN;
+    long long intervalTime = (long long)intervalSeconds * 1000000000 + intervalNano;
+    
+    return (currentTime - lastLaunchTime) >= intervalTime;
+}
+
+void printProcessTable(SimulatedClock* clock) 
+{
+    ostringstream oss;
+    oss << "OSS PID:" << getpid() << " SysClockS: " << clock->seconds
+        << " SysclockNano: " << clock->nanoSeconds << endl;
+    oss << "Process Table:" << endl;
+    oss << "Entry Occupied PID    StartS StartN MessagesSent" << endl;
+    for (int i = 0; i < 20; i++) 
+    {
+        oss << setw(5) << i << " "
+            << setw(8) << processTable[i].occupied << " "
+            << setw(6) << processTable[i].pid << " "
+            << setw(6) << processTable[i].startSeconds << " "
+            << setw(6) << processTable[i].startNano << " "
+            << setw(12) << processTable[i].messagesSent << endl;
+    }
+    oss << endl;
+    log_output(oss.str());
+}
+
+int findFreeSlot() 
+{
+    for (int i = 0; i < 20; i++) 
+    {
+        if (processTable[i].occupied == 0) 
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void launchWorker(int slot, double timeLimit, SimulatedClock* clock) 
+{
+    // random time between 1 and timeLimit
+    int timeLimitSeconds = rand() % ((int)timeLimit) + 1;
+    int timeLimitNano = rand() % 1000000000;
+    
+    pid_t pid = fork();
+    if (pid == 0) 
+    {
+        // child process
+        string secondsStr = to_string(timeLimitSeconds);
+        string nanoStr = to_string(timeLimitNano);
+        execl("./worker", "worker", secondsStr.c_str(), nanoStr.c_str(), nullptr);
+        perror("execl failed");
+        exit(1);
+    } else if (pid > 0) 
+    {
+        // parent process
+        processTable[slot].occupied = 1;
+        processTable[slot].pid = pid;
+        processTable[slot].startSeconds = clock->seconds;
+        processTable[slot].startNano = clock->nanoSeconds;
+        processTable[slot].messagesSent = 0;
+        
+        ostringstream oss;
+        oss << "OSS: Generating process with PID " << pid << " and putting it in process table at index " << slot << " at time "
+            << clock->seconds << ":" << clock->nanoSeconds << endl;
+        log_output(oss.str());
+        
+        printProcessTable(clock);
+    } else {
+        perror("fork failed");
+    }
+}
