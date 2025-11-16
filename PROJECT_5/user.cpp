7250c1adafd8ea8653fa7ab9e273f16cb42ece35
Grant Hughes <granthughes636@gmail.com>
2025-11-16 16:51:22 -0600

    finished

--- PROJECT_5/user.cpp
+++ PROJECT_5/user.cpp
@@ -17,6 +17,7 @@ File: Where child processes request/release resources
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    // DEBUG: Uncomment for debugging
     // cerr << "USER PROCESS STARTED with " << argc << " arguments" << endl;
     // cerr.flush();
     
@@ -29,64 +30,72 @@ int main(int argc, char* argv[]) {
     int processIndex = atoi(argv[1]);
     int timeLimit = atoi(argv[2]);
     
+    // DEBUG: Uncomment for debugging
     // cerr << "USER: processIndex=" << processIndex << " timeLimit=" << timeLimit << endl;
     // cerr.flush();
     
-    // seed random number generator
+    // Seed random number generator
     srand(time(nullptr) ^ (getpid() << 16));
-
+    
+    // DEBUG: Uncomment for debugging
     // cerr << "USER: Attaching to shared memory..." << endl;
     // cerr.flush();
     
-    // attach to shared memory
+    // Attach to shared memory
     int shmClock = shmget(CLOCK_KEY, sizeof(Clock), 0666);
     int shmResource = shmget(RESOURCE_KEY, sizeof(ResourceDescriptor) * NUM_RESOURCES, 0666);
     int shmPCB = shmget(PROCESS_TABLE_KEY, sizeof(PCB) * MAX_PROCESSES, 0666);
     int msgQueue = msgget(MSG_QUEUE_KEY, 0666);
     
+    // DEBUG: Uncomment for debugging
     // cerr << "USER: shmget results: clock=" << shmClock << " resource=" << shmResource 
     //      << " pcb=" << shmPCB << " msg=" << msgQueue << endl;
     
     if (shmClock == -1 || shmResource == -1 || shmPCB == -1 || msgQueue == -1) {
         cerr << "P" << processIndex << ": Failed to attach to shared memory\n";
         perror("shmget/msgget error");
-
         return 1;
     }
+    
+    // DEBUG: Uncomment for debugging
     // cerr << "USER: Calling shmat..." << endl;
     
     Clock* sharedClock = (Clock*)shmat(shmClock, nullptr, 0);
     ResourceDescriptor* resources = (ResourceDescriptor*)shmat(shmResource, nullptr, 0);
     PCB* processTable = (PCB*)shmat(shmPCB, nullptr, 0);
     
+    // DEBUG: Uncomment for debugging
     // cerr << "USER: shmat complete, pointers: clock=" << sharedClock 
     //      << " resources=" << resources << " processTable=" << processTable << endl;
     
     if (sharedClock == (void*)-1 || resources == (void*)-1 || processTable == (void*)-1) {
         cerr << "P" << processIndex << ": shmat failed\n";
         perror("shmat error");
-
         return 1;
     }
+    
+    // DEBUG: Uncomment for debugging
     // cerr << "USER: Successfully attached to all shared memory" << endl;
     // cerr.flush();
     
-    // delay for parent to finish PCB initialization
+    // Brief delay for parent to finish PCB initialization
     usleep(1000);  // Just 1ms
     
-    // if clock seems invalid, wait a bit more
+    // Sanity check - if clock seems invalid, wait a bit more
     if (sharedClock->seconds > 100000) {
+        // DEBUG: Uncomment for debugging
         // cerr << "P" << processIndex << " waiting for clock initialization..." << endl;
         usleep(50000);
     }
-    // tracking my resources
+    
+    // Track my resources
     int myResources[NUM_RESOURCES] = {0};
     
-    // calculating termination time
+    // Calculate termination time
     unsigned int terminateSeconds = processTable[processIndex].startSeconds + timeLimit;
     unsigned int terminateNanoSeconds = processTable[processIndex].startNanoSeconds;
     
-    // calculating next action time (random 0-100ms from current clock)
+    // Calculate next action time (random 0-100ms from current clock)
     unsigned int currentSeconds = sharedClock->seconds;
     unsigned int currentNanoSeconds = sharedClock->nanoSeconds;
     
@@ -96,28 +105,40 @@ int main(int argc, char* argv[]) {
         nextActionSeconds++;
         nextActionNanoSeconds -= 1000000000;
     }
+    
+    // DEBUG: Uncomment for debugging
     // cerr << "P" << processIndex << " initialized: will terminate at " 
     //      << terminateSeconds << ":" << terminateNanoSeconds 
     //      << ", first action at " << nextActionSeconds << ":" << nextActionNanoSeconds 
     //      << " (current time: " << currentSeconds << ":" << currentNanoSeconds << ")" << endl;
     
+    // Main loop
+    int loopCount = 0;
     while (1) {
-        // checking if time to terminate
-        if (timePassed(sharedClock, terminateSeconds, terminateNanoSeconds)) {
-            // cerr << "P" << processIndex << " reached time limit, terminating" << endl;
-            // cerr.flush();
-            
-            Message msg;
-            msg.mtype = 1;  // message type must be > 0
-            msg.requestType = MSG_TERMINATE;
-            msg.processIndex = processIndex;
-            memcpy(msg.resources, myResources, sizeof(int) * NUM_RESOURCES);
-            msgsnd(msgQueue, &msg, sizeof(Message) - sizeof(long), 0);
-            break;
+        // Force termination check every 100 iterations even if time hasn't passed
+        loopCount++;
+        bool forceTermCheck = (loopCount % 100 == 0);
+        
+        // Check if time to terminate
+        if (forceTermCheck || timePassed(sharedClock, terminateSeconds, terminateNanoSeconds)) {
+            if (timePassed(sharedClock, terminateSeconds, terminateNanoSeconds)) {
+                // DEBUG: Uncomment for debugging
+                // cerr << "P" << processIndex << " reached time limit, terminating" << endl;
+                // cerr.flush();
+                
+                Message msg;
+                msg.mtype = 1;  // Message type must be > 0
+                msg.requestType = MSG_TERMINATE;
+                msg.processIndex = processIndex;
+                memcpy(msg.resources, myResources, sizeof(int) * NUM_RESOURCES);
+                msgsnd(msgQueue, &msg, sizeof(Message) - sizeof(long), 0);
+                break;
+            }
         }
         
-        // checking if time for next action
+        // Check if time for next action
         if (timePassed(sharedClock, nextActionSeconds, nextActionNanoSeconds)) {
+            // DEBUG: Uncomment for debugging
             // cerr << "P" << processIndex << " taking action at " 
             //      << sharedClock->seconds << ":" << sharedClock->nanoSeconds << endl;
             
@@ -125,26 +146,26 @@ int main(int argc, char* argv[]) {
             bool shouldRequest = (rand() % 100) < 60;
             
             if (shouldRequest) {
-                // random resource to request
+                // Choose a random resource to request
                 int resourceToRequest = rand() % NUM_RESOURCES;
                 int amountToRequest = 1 + (rand() % 2);  // Request 1-2 instances
                 
-                // we don't exceed maximum instances
+                // Make sure we don't exceed maximum instances
                 if (myResources[resourceToRequest] + amountToRequest > INSTANCES_PER_RESOURCE) {
                     amountToRequest = INSTANCES_PER_RESOURCE - myResources[resourceToRequest];
                 }
                 
                 if (amountToRequest > 0) {
-                    // checking if this violates resource ordering
+                    // Check if this violates resource ordering
                     bool needsMassRelease = false;
                     int releaseResources[NUM_RESOURCES] = {0};
                     
-                    // find if we have any resources numbered higher than what we're requesting
+                    // Find if we have any resources numbered higher than what we're requesting
                     // and we also have resources numbered lower
                     for (int i = 0; i < resourceToRequest; i++) {
                         if (myResources[i] > 0) {
-                            // lower numbered resource
-                            // checking if we have any higher numbered ones
+                            // We have a lower numbered resource
+                            // Check if we have any higher numbered ones
                             for (int j = resourceToRequest + 1; j < NUM_RESOURCES; j++) {
                                 if (myResources[j] > 0) {
                                     needsMassRelease = true;
@@ -156,11 +177,12 @@ int main(int argc, char* argv[]) {
                     }
                     
                     if (needsMassRelease) {
-                        // release all resources numbered less than what we want
+                        // Release all resources numbered less than what we want
                         for (int i = 0; i < resourceToRequest; i++) {
                             releaseResources[i] = myResources[i];
                         }
-                        // sending release message
+                        
+                        // Send release message
                         Message releaseMsg;
                         releaseMsg.mtype = 1;  // Must be > 0
                         releaseMsg.requestType = MSG_RELEASE;
@@ -168,15 +190,16 @@ int main(int argc, char* argv[]) {
                         memcpy(releaseMsg.resources, releaseResources, sizeof(int) * NUM_RESOURCES);
                         msgsnd(msgQueue, &releaseMsg, sizeof(Message) - sizeof(long), 0);
                         
-                        // waiting for acknowledgment
+                        // Wait for acknowledgment
                         Message ack;
                         msgrcv(msgQueue, &ack, sizeof(Message) - sizeof(long), getpid(), 0);
                         
-                        // updating my resource counts
+                        // Update my resource counts
                         for (int i = 0; i < NUM_RESOURCES; i++) {
                             myResources[i] -= releaseResources[i];
                         }
-                        // request everything back plus the new resource
+                        
+                        // Now request everything back plus the new resource
                         int requestResources[NUM_RESOURCES] = {0};
                         for (int i = 0; i < resourceToRequest; i++) {
                             requestResources[i] = releaseResources[i];
@@ -190,16 +213,16 @@ int main(int argc, char* argv[]) {
                         memcpy(requestMsg.resources, requestResources, sizeof(int) * NUM_RESOURCES);
                         msgsnd(msgQueue, &requestMsg, sizeof(Message) - sizeof(long), 0);
                         
-                        // waiting for grant
+                        // Wait for grant
                         Message grant;
                         msgrcv(msgQueue, &grant, sizeof(Message) - sizeof(long), getpid(), 0);
                         
-                        // updating my resources
+                        // Update my resources
                         for (int i = 0; i < NUM_RESOURCES; i++) {
                             myResources[i] += requestResources[i];
                         }
                     } else {
-                        // simple request
+                        // Simple request
                         int requestResources[NUM_RESOURCES] = {0};
                         requestResources[resourceToRequest] = amountToRequest;
                         
@@ -209,6 +232,7 @@ int main(int argc, char* argv[]) {
                         requestMsg.processIndex = processIndex;
                         memcpy(requestMsg.resources, requestResources, sizeof(int) * NUM_RESOURCES);
                         
+                        // DEBUG: Uncomment for debugging
                         // cerr << "P" << processIndex << " sending REQUEST for R" << resourceToRequest 
                         //      << ":" << amountToRequest << endl;
                         // cerr << "DEBUG: mtype=" << requestMsg.mtype << " requestType=" << requestMsg.requestType 
@@ -220,11 +244,13 @@ int main(int argc, char* argv[]) {
                         
                         int msgsnd_result = msgsnd(msgQueue, &requestMsg, sizeof(Message) - sizeof(long), 0);
                         
+                        // DEBUG: Uncomment for debugging
                         // cerr << "DEBUG: msgsnd returned " << msgsnd_result << endl;
                         // cerr.flush();
                         
                         if (msgsnd_result == -1) {
                             perror("P: msgsnd failed");
+                            // DEBUG: Uncomment for debugging
                             // cerr << "P" << processIndex << " msgsnd failed! errno=" << errno << " (" << strerror(errno) << ")" << endl;
                             // struct msqid_ds buf;
                             // if (msgctl(msgQueue, IPC_STAT, &buf) == 0) {
@@ -235,15 +261,17 @@ int main(int argc, char* argv[]) {
                             // cerr.flush();
                             break;
                         }
+                        // DEBUG: Uncomment for debugging
                         // else {
                         //     cerr << "P" << processIndex << " msgsnd successful, waiting for reply..." << endl;
                         //     cerr.flush();
                         // }
                         
-                        // waiting for grant (might be blocked)
+                        // Wait for grant (might be blocked)
                         Message grant;
                         pid_t myPid = getpid();
                         
+                        // DEBUG: Uncomment for debugging
                         // cerr << "P" << processIndex << " (PID=" << myPid << ") calling msgrcv for reply (waiting for mtype=" << myPid << ")..." << endl;
                         // cerr.flush();
                         
@@ -251,25 +279,30 @@ int main(int argc, char* argv[]) {
                         
                         if (recv_result == -1) {
                             perror("P: msgrcv failed");
+                            // DEBUG: Uncomment for debugging
                             // cerr << "P" << processIndex << " msgrcv failed! errno=" << errno << " (" << strerror(errno) << ")" << endl;
                             // cerr.flush();
                             break;
                         }
+                        
+                        // DEBUG: Uncomment for debugging
                         // cerr << "P" << processIndex << " received reply (recv_result=" << recv_result << ", grant.mtype=" << grant.mtype << ")" << endl;
                         // cerr.flush();
                         
                         myResources[resourceToRequest] += amountToRequest;
                         
+                        // DEBUG: Uncomment for debugging
                         // cerr << "P" << processIndex << " got R" << resourceToRequest 
                         //      << ":" << amountToRequest << endl;
                     }
                 }
             } else {
-                // releasing a random resource
+                // Release a random resource
                 int totalResources = 0;
                 for (int i = 0; i < NUM_RESOURCES; i++) {
                     totalResources += myResources[i];
                 }
+                
                 if (totalResources > 0) {
                     // Pick a resource we have
                     int resourceToRelease = -1;
@@ -280,6 +313,7 @@ int main(int argc, char* argv[]) {
                             }
                         }
                     }
+                    
                     if (resourceToRelease != -1) {
                         int amountToRelease = 1 + (rand() % myResources[resourceToRelease]);
                         
@@ -293,18 +327,20 @@ int main(int argc, char* argv[]) {
                         memcpy(releaseMsg.resources, releaseResources, sizeof(int) * NUM_RESOURCES);
                         msgsnd(msgQueue, &releaseMsg, sizeof(Message) - sizeof(long), 0);
                         
-                        // wating for acknowledgment
+                        // Wait for acknowledgment
                         Message ack;
                         msgrcv(msgQueue, &ack, sizeof(Message) - sizeof(long), getpid(), 0);
                         
                         myResources[resourceToRelease] -= amountToRelease;
                         
+                        // DEBUG: Uncomment for debugging
                         // cerr << "P" << processIndex << " released R" << resourceToRelease 
                         //      << ":" << amountToRelease << endl;
                     }
                 }
             }
-            // calculate next action time
+            
+            // Calculate next action time
             nextActionSeconds = sharedClock->seconds;
             nextActionNanoSeconds = sharedClock->nanoSeconds + (rand() % 100000000);
             while (nextActionNanoSeconds >= 1000000000) {
@@ -312,10 +348,12 @@ int main(int argc, char* argv[]) {
                 nextActionNanoSeconds -= 1000000000;
             }
         }
-        // sleep to avoid busy waiting
+        
+        // Small sleep to avoid busy waiting
         usleep(100);  // 100 microseconds for responsiveness
     }
-    // detach shared memory
+    
+    // Detach shared memory
     shmdt(sharedClock);
     shmdt(resources);
     shmdt(processTable);
