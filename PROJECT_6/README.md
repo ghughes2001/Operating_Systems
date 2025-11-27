59a4aaadab19cad81e5c24a9feeef6413bd91e92
Grant Hughes <granthughes636@gmail.com>
2025-11-27 11:06:06 -0600

    added new project

--- PROJECT_6/README.md
+++ PROJECT_6/README.md
@@ -3,13 +3,12 @@ Date: October 8, 2025
 Environment: (Mac, Visual Studio Code)
 How to compile the project: Type ’make’
 Example of how to run the project: 
-NOTES:
-I created degub files caused I could not figure out for the life of me (for days) what the issue was. I turned to AI for some of the debugging.
+
 
 Generative AI used: CHatGPT
 Prompts:
-deadlock prevent strategies and show code in c++
-more code about requesting and releasing
+example of code that use paging
+this project looks complex, what is th best way to approach it
 
 Git control: Github
 Link: (Project is the PROJECT_3) -> https://github.com/ghughes2001/Operating_Systems.git
---
+++ PROJECT_6/shared.h
@@ -0,0 +1,92 @@
+/*
+Auhtor: GRant Hughes
+Date: Nov 27, 2025
+File: file that holds resources for the main files
+*/
+
+#ifndef SHARED_H
+#define SHARED_H
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <ctime>
+
+// constants
+#define MAX_PROCESSES 18
+#define MEMORY_SIZE 65536        // 64K total memory
+#define PAGE_SIZE 1024           // 1K page size
+#define PROCESS_MEMORY 16384     // 16K per process
+#define PAGES_PER_PROCESS 16     // 16 pages per process
+#define TOTAL_FRAMES 64          // 64 frames total
+#define DISK_IO_TIME_NS 14000000 // 14ms in nanoseconds
+
+// simulated clock structure
+struct Clock
+{
+    unsigned int seconds;
+    unsigned int nanoseconds;
+};
+
+// frame table entry
+struct Frame
+{
+    bool occupied;
+    bool dirtyBit;
+    int processId;
+    int pageNumber;
+    unsigned int loadTime;  // When this frame was loaded (for FIFO)
+};
+
+// page table for each process
+struct PageTableEntry
+{
+    int frameNumber;  // -1 if not in memory
+    bool valid;
+    bool dirty;
+};
+
+// Process Control Block
+struct PCB
+{
+    pid_t pid;
+    bool inUse;
+    int processId;
+    unsigned int startTime;
+    unsigned int totalMemoryAccesses;
+    unsigned int pageFaults;
+    unsigned long long totalAccessTime;
+    PageTableEntry pageTable[PAGES_PER_PROCESS];
+};
+
+// message structure for IPC
+struct Message
+{
+    long mtype;
+    int processId;
+    int address;
+    bool isWrite;
+    bool terminate;
+    bool granted;
+};
+
+// shared memory structure
+struct SharedMemory
+{
+    Clock clock;
+    PCB processTable[MAX_PROCESSES];
+    Frame frameTable[TOTAL_FRAMES];
+};
+
+// blocked processes
+struct BlockedProcess
+{
+    int processId;
+    int address;
+    bool isWrite;
+    unsigned long long unblockTime;
+    BlockedProcess* next;
+};
+
+#endif
